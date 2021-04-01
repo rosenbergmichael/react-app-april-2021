4e254ae256da0577bed9ee8b753491c3b1c42f86
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-04-01 18:29:18 -0400

    in App.js import BrowserRouter as Router, Switch and Route from react-router-dom, in App component add Router and Switch, create Route for '/'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact />
+        </Switch>
+      </Router>  
+    </>
   );
 }
 
