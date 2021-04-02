9970b13382d5e0260a3f86551c1419919c594ebb
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-04-02 09:18:34 -0400

    in Navbar.js add Link with onClick closeMobileMenu, create that function set to setClick(false)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,17 +5,25 @@ function Navbar() {
   const [click, setClick] = useState(false);
 
   const handleClick = () => setClick(!click);
-  
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <>
-      <nav className="navbar">
-        <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
-            TopTravel <i className="fas fa-passport" />
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <Link to="/" className='navbar-logo'>
+            TopTravel <i className='fas fa-passport' />
           </Link>
-          <div className="menu-icon" onClick={handleClick}> 
-            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          <div className='menu-icon' onClick={handleClick}> 
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
           </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item '>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+          </ul>
         </div>       
       </nav>
     </>
