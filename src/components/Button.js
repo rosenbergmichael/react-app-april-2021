9d4074ca7379f936cae524e107cddd4262b98462
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-04-03 12:22:21 -0400

    finish building out Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -15,7 +15,17 @@ export const Button = ({
 }) => {
   const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
   const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+
   return (
-    
-  )
+    <Link to='/sign-up' className='btn-mobile'>
+      <button
+      className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+      onClick={onclick}
+      type={type}
+      >
+        {children}
+      </button>
+    </Link>
+  
+  );
 };
