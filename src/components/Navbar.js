5661f4ad7c08edcbedd50e296b4faf9f53dea4b6
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-04-03 12:56:18 -0400

    in Navbar.js change order of logo and App name, edit padding of logo in Navbar.css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-import {Button} from './Button.js';
+import {Button} from './Button';
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -26,7 +26,7 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to="/" className='navbar-logo'>
-            TopTravel <i className='fas fa-passport' />
+            <i className='fas fa-passport' />TopTravel
           </Link>
           <div className='menu-icon' onClick={handleClick}> 
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
