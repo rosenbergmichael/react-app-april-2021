ebbee33bbca12f2c10fe8aa0b02d59901cc100e0
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-04-09 12:04:14 -0400

    in App.js import Destinations, About and SignUp components, create Routes for those paths

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/pages/Home';
+import Destinations from './components/pages/Destinations';
+import About from './components/pages/About';
+import SignUp from './components/pages/SignUp';
 import './App.css';
 
 function App() {
@@ -11,6 +14,9 @@ function App() {
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
+          <Route path='/destinations' component={Destinations} />
+          <Route path='/about' component={About} />
+          <Route path='/sign-up' component={SignUp} />
         </Switch>
       </Router>  
     </>
