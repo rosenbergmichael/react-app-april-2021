7ad90eb222e73447c68a3841ee9e6103f4cc1bc3
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-04-12 09:08:11 -0400

    in Footer.js add p tags for Subscribe and Unsubscribe, create form with input for email address, import Button component and render Button for Subscribe

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Button } from './Button'
 
 function Footer() {
   return (
     <div className='footer-container'>
-      <section className="footer-subscription"></section>
+      <section className="footer-subscription">
+        <p className="footer-subscription-heading">
+          Join the newsletter and don't miss out!
+        </p>
+        <p className="footer-subscription-text">
+          Unsubscribe at any time.
+        </p>
+        <div className="input-areas">
+          <form>
+            <input type="email" name="email" placeholder="youremail@example.com" className="footer-input">
+              <Button buttonStyle='btn--outline'>Subscribe</Button>
+          </form>
+        </div>
+      </section>
     </div>
   )
 }
