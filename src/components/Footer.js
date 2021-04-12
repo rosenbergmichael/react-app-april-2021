2443ab83c418045fb8003dbe22a16ec1ddcb1990
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-04-12 08:58:01 -0400

    in Footer.js import React, create and export Footer component, add className footer-container to div, create section tag with className footer-subscription

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+      <section className="footer-subscription"></section>
+    </div>
+  )
+}
+
+export default Footer
