f79d10031555c3a647a5e1cb17b46c49c0a854ed
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-05-12 11:33:13 -0400

    create SignUp.css file, add styling for .sign-up-form

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../../App.css';
 import Footer from '../Footer';
+import '../../components/SignUp.css';
 
 export default function SignUp() {
   return ( 
     <div>
       <img src="/images/signup2.jpeg" alt="signup-img" />
-      <h1 className="sign-up">SIGN UP</h1>;
+      <h1 className="sign-up">SIGN UP</h1>
       <form className="sign-up-form">
         <input type="text" placeholder="Name"/>
         <input type="text" placeholder="Email"/>
